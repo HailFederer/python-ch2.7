16d07f7917b92dc4ae4c01f189c4cbc69267c3dc
HailFederer <urimaumdero@naver.com>
2017-10-10 18:45:16 +0900

    Initial commit

---
+++ import.py
@@ -0,0 +1,8 @@
+# 다양한 import 방법
+
+# import math
+# print(math.sin(math.pi/6), math.cos(math.pi/3), math.tan(math.pi/4))
+
+from math import pi, sin, cos, tan
+# from mymath import *
+print(sin(pi/6), cos(pi/3), tan(pi/4))
---
+++ test_mymath.py
@@ -0,0 +1,8 @@
+import sys
+# sys.path.append('/bigdata/PycharmProjects/python_modules')
+
+import mymath
+
+# print(sys.path)
+print(mymath.add(10, 20))
+print(mymath.area_circle(4))
